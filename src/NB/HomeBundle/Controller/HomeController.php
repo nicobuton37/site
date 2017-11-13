e9d4a1ee7787632ca4dc3fe58244adeaa780ced3
nicobuton37 <nicobuton370@gmail.com>
2017-11-13 16:20:22 +0100

    create different pages

--- src/NB/HomeBundle/Controller/HomeController.php
+++ src/NB/HomeBundle/Controller/HomeController.php
@@ -13,4 +13,24 @@ class HomeController extends Controller
   {
     return $this->get('templating')->renderResponse('NBHomeBundle:Home:index.html.twig');
   }
+
+  public function contactAction()
+  {
+    return $this->get('templating')->renderResponse('NBHomeBundle:Contact:contact.html.twig');
+  }
+
+  public function aboutAction()
+  {
+    return $this->get('templating')->renderResponse('NBHomeBundle:About:about.html.twig');
+  }
+
+  public function portfolioAction()
+  {
+    return $this->get('templating')->renderResponse('NBHomeBundle:Portfolio:portfolio.html.twig');
+  }
+
+  public function blogAction()
+  {
+    return $this->get('templating')->renderResponse('NBHomeBundle:Blog:blog.html.twig');
+  }
 }
